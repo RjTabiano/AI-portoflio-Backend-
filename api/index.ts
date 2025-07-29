6ae87bf75f1b8597910c19f6ea6bdaff88270de8
RjTabiano <rj.tabiano@hoolisoftware.com>
2025-07-29 15:42:53 +0800

    Fix server intialization for vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import app from '../src/index.js';
+import app from '../src/index';
 import { IncomingMessage, ServerResponse } from 'http';
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
-  (app as any).handle(req, res); 
+  (app as any).handle(req, res);
 }
